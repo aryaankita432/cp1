340c6b29e570746c965258bb33ef5883cd56c8be
Ankita Arya <aryaankita432@gmail.com>
2022-01-05 00:17:53 +0530

    potd 4 04/01/2022

--- potd4.cpp
+++ potd4.cpp
@@ -1,37 +1,27 @@
 #include<bits/stdc++.h>
 using namespace std;
-void prime(int);
-bool isprime[100001];
-void prime(int n)
-{
-    memset(isprime,true,sizeof(isprime));
-    for(int i=2;i*i<=n;i++)
-    {
-        if(isprime[i])
-        {
-            for(int j=i*i;j<=n;j+=i)
-            {
-                isprime[j]=false;
-            }
-        }
-    }
-    
-}
 string solve (int N) {
-   if(N<=3)
-   return "No";
-   if(isprime[N])
-   return "No";
-   else
-   return "Yes";
-
+    int c=0;
+    for(int i=2;i<=sqrt(N);i++)
+    {
+        if(N%i==0)
+        {
+            c++;
+            break;
+        }
+       
+    }
+    if(c==0)
+    return "No";
+    else if(c==1)
+    return "Yes";
+ 
 }
-
+ 
 int main() {
-
+ 
     ios::sync_with_stdio(0);
     cin.tie(0);
-    prime(100000);
     int T;
     cin >> T;
     for(int t_i=0; t_i<T; t_i++)
